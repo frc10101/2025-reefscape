2c767dc934a2e24bad585066ab4dc727c8500d9d
NotMaxwell <William.mccormick@ascte.org>
2025-02-14 15:05:16 -0600

    Arm updated to do arm things

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,94 +4,57 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.pathplanner.lib.config.PIDConstants;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.servohub.ServoHub.ResetMode;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.Encoder;
-import com.revrobotics.spark.SparkBase.ControlType;
-
 public class Arm extends SubsystemBase {
-  private final double kMaxVelocity = 1.75;
-  private final double kMaxAcceleration = 0.75;
-  private final double kP = 1.3;
+  private SparkMax m_armMotor;
+  private final double kMaxVelocity = 2.0;
+  private final double kMaxAcceleration = 1.0;
+  private final double kP = 1.0;
   private final double kI = 0.0;
-  private final double kD = 0.7;
-  private final double kFF = 1.1;
+  private final double kD = 0.5;
+  private final double kFF = 0.8;
 
-  private SparkMax armMotor;
-
-  private RelativeEncoder armEncoder;
-
-  private PIDController armController;
-
-  private double armPosition;
-
-  private SparkMaxConfig armMotorConfig;
-
-  private SparkClosedLoopController armClosedLoopController;
-
+  /** Creates a new arm. */
   public Arm() {
+    m_armMotor = new SparkMax(2, MotorType.kBrushless);
     SparkMaxConfig config = new SparkMaxConfig();
-    config.closedLoop
-        .pidf(kP, kI, kD, kFF);
-  config.closedLoop.maxMotion
-        .maxAcceleration(kMaxAcceleration)
-        .maxVelocity(kMaxVelocity)
-        .positionMode(MAXMotionPositionMode.kMAXMotionTrapezoidal);
 
-  private SparkMaxConfig armMotorConfig;
+    config.closedLoop.pidf(kP, kI, kD, kFF);
 
-  public Arm() {
-    armMotorConfig = new SparkMaxConfig();
-    armMotorConfig
+    config
         .closedLoop
-        .pidf(kP, kI, kD, kFF)
         .maxMotion
         .maxAcceleration(kMaxAcceleration)
         .maxVelocity(kMaxVelocity)
         .positionMode(MAXMotionPositionMode.kMAXMotionTrapezoidal);
 
-  config.encoder
-        .positionConversionFactor(2.0 * Math.PI * 1.5);
+    config.encoder.positionConversionFactor(2.0 * Math.PI * 1.0);
 
-  armMotor = new SparkMax(1, MotorType.kBrushless);
-  armMotor.configure(
-        config,
-        ResetMode.kResetSafeParameters,
-        PersistMode.kPersistParameters);
+    m_armMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    config.closedLoop.pidf(kP, kI, kD, kFF);
+  }
+
+  private void moveToPosition(double position) {
+    m_armMotor
+        .getClosedLoopController()
+        .setReference(position, ControlType.kMAXMotionPositionControl);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-    armMotorConfig.encoder.positionConversionFactor(2.0 * Math.PI * 1.5);
 
-    armMotor = new SparkMax(1, MotorType.kBrushless);
-    armMotor.configure(
-        armMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  public Command armToPosition(double position) {
+    return run(() -> moveToPosition(position));
   }
-
-  private void goToGoal(double goal) {
-    armMotor.getClosedLoopController().setReference(goal, ControlType.kMAXMotionPositionControl);
-  }
-
-  public Command move(double goal) {
-    return run(() -> goToGoal(goal));
-  }
-
-  @Override
-  public void periodic() {}
 }
