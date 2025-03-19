0bf02d7d18c268235f46189e950ca1fed93b7be6
Cohen Hill <cohen.hill@ascte.org>
2025-03-18 19:14:30 -0500

    fix deps

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -120,5 +120,6 @@ public class Elevator extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    System.out.println(m_motorLeft.getOutputCurrent());
   }
 }
