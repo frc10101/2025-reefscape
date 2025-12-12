42d76ed2e9c205daa2d88928a7405c9d9376e41b
KaosMaker9 <nsifuentes09@gmail.com>
2025-12-11 20:05:02 -0600

    It's fully not a command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -181,7 +182,7 @@ public class RobotContainer {
 
     button3.onTrue(drive.reLocalize());
     button11.onTrue(
-        Commands.runOnce(() -> new AlignToPose(drive, () -> getCurrentPoseFromLimeLight(), false))
+        new AlignToPose(drive, () -> getCurrentPoseFromLimeLight(), false)
         );
   }
 
