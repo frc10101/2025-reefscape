32a3b0d0c0fddf8e84dfb48bd186499d1a382b62
jason <jasonjeontaekang@gmail.com>
2025-12-11 20:11:55 -0600

    Spotless Applied

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,6 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -181,9 +180,7 @@ public class RobotContainer {
     button10.onTrue(elevator.L1());
 
     button3.onTrue(drive.reLocalize());
-    button11.onTrue(
-        new AlignToPose(drive, () -> getCurrentPoseFromLimeLight(), false)
-        );
+    button11.onTrue(new AlignToPose(drive, () -> getCurrentPoseFromLimeLight(), false));
   }
 
   private void configureSwerveCommands() {
